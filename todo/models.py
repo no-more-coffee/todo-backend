926334ffdaf55bb3ee5e9dcfe00d070704726176
no-more-coffee <nomorecoffee@yandex.ru>
2020-03-16 09:42:26 +0500

    Add models and admin.

--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+
+class Todo(models.Model):
+    name = models.CharField(max_length=1024)
+    is_done = models.BooleanField(default=False)
+    tags = models.ForeignKey(
+        'todo.Tag',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -24,13 +24,13 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'todo.apps.TodoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo.apps.TodoConfig',
 ]
 
 MIDDLEWARE = [
