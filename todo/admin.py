4fca3c053ebee46a63eaaa3ff67efc56ff7af832
no-more-coffee <nomorecoffee@yandex.ru>
2020-03-17 11:35:02 +0500

    Add pretty admin views.

--- todo/admin.py
+++ todo/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from todo.models import Todo, Tag
 
-admin.site.register(Todo)
-admin.site.register(Tag)
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_done', 'created', 'modified')
+    search_fields = ('id', 'name')
+    list_filter = ('is_done', 'tags')
+    fields = ('name', 'is_done', 'tags', 'created', 'modified')
+    readonly_fields = ('created', 'modified')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('id', 'name')
+
--- todo/models.py
+++ todo/models.py
@@ -4,15 +4,16 @@ from django.db import models
 class Todo(models.Model):
     name = models.CharField(max_length=1024)
     is_done = models.BooleanField(default=False)
-    tags = models.ForeignKey(
-        'todo.Tag',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
-    )
+    tags = models.ManyToManyField('todo.Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.id} {self.name[:64]}'
+
 
 class Tag(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
