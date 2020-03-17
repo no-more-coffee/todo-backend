d48fa28eebd9040f83d20c91feb28502f4b745a3
no-more-coffee <nomorecoffee@yandex.ru>
2020-03-17 11:40:54 +0500

    Add tags to admin list view.

--- todo/admin.py
+++ todo/admin.py
@@ -5,15 +5,18 @@ from todo.models import Todo, Tag
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'is_done', 'created', 'modified')
+    list_display = ('id', 'name', 'is_done', 'created', 'modified', 'get_tags')
     search_fields = ('id', 'name')
     list_filter = ('is_done', 'tags')
     fields = ('name', 'is_done', 'tags', 'created', 'modified')
     readonly_fields = ('created', 'modified')
+
+    def get_tags(self, instance: Todo):
+        return ';'.join(instance.tags.values_list('name', flat=True))
+    get_tags.short_description = 'Tags'
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('id', 'name')
-
